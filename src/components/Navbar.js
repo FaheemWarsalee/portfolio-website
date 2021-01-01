4cc76849c8880165c7df033b90866a5cee28657d
Faheem Warsalee <warsfa@gmail.com>
2021-01-01 14:31:54 -0500

    Added CSS for Navbar.css and App.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,9 +13,9 @@ function Navbar() {
   // 
   const showButton = () => {
     if(window.innerWidth <= 960){
-      setButton = false;
+      setButton(false);
     } else {
-      setButton = true;
+      setButton(true);
     }
   }
 
