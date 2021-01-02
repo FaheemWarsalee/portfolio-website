ab771c9ad68d7e79dd54dd54b9f531b251b5b3f8
Faheem Warsalee <warsfa@gmail.com>
2021-01-02 15:05:37 -0500

    finalized hero/main page component.

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import Button from './Button';
-import './App.css';
+import { Button } from './Button';
+import '../App.css';
 import './HeroSection.css';
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <img src="public/Japan gates.JPG"></img>
 
       <h1>Faheem Warsalee</h1>
       <p>Second Year Computer Science Student</p>
 
       <div className="hero-btns">
-        <Button className="btns" buttonStyle="btn--outline" buttonSize='btn--large'> About Me </Button>
-        <Button className="btns" buttonStyle="btn--outline" buttonSize='btn--primary'> Projects </Button>
+        <Button className="btns" buttonStyle="btn--outline"buttonSize="btn--large"> About Me </Button>
+        <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large"> Projects </Button>
       </div>
     </div>
   );
