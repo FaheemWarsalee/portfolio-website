3f3427900a6889f08737a761c5445ead642da591
Faheem Warsalee <warsfa@gmail.com>
2021-01-03 14:21:58 -0500

    Trimmed and tweaked footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,39 @@
 import React from 'react'
-import {Button} from '../components/Button';
 import '../css/Footer.css';
 
 function Footer() {
   return (
     <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">Reach Me At...</p>
-        <p className="footer-subscription-text">
-          faheem.warsalee@gmail.com | https://github.com/FaheemWarsalee |
-          https://www.linkedin.com/in/faheem-warsalee-92a013203/
-        </p>
-
-        <div className="input-areas">
-          <form>
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              className="footer-input"
-            />
-            <Button buttonStyle="btn--outline">Contact Me</Button>
-          </form>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Reach me at...</h2>
+            <a
+              href="mailto:faheem.warsalee@gmail.com"
+              targe="_blank"
+              rel="noopener noreferrer"
+            >
+              Email
+            </a>
+            <a
+              href="https://github.com/FaheemWarsalee"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://www.linkedin.com/in/faheem-warsalee-92a013203/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
         </div>
-      </section>
+      </div>
+
+      <p className="copyright-sign"> © Faheem 2021</p>
     </div>
   );
 }
